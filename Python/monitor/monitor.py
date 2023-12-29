bf4b98a437ec443fc5904faa2ddd8ba87868641a
djyanke <92189196+djyanke@users.noreply.github.com>
2023-12-29 00:36:08 -0500

    Working Monitor Script

---
+++ Python/monitor/monitor.py
@@ -0,0 +1,82 @@
+from twilio.rest import Client
+import os
+import datetime
+import time
+from dateutil import parser
+
+os.chdir(os.path.dirname(__file__))
+
+data_dir = ""
+params_dir = ""
+
+if os.path.exists(os.path.abspath("../../../data")) and os.path.exists(os.path.abspath("../../../params")):
+    data_dir = os.path.abspath("../../../data")+"/"
+    params_dir = os.path.abspath("../../../params")+"/"
+    print("Found Data and Parameter directories")
+else:
+    print("Data or Parameter directories not found.")
+    data_dir = None
+    params_dir = None
+
+main_params = {}
+
+def get_greenhouse_data():
+    file_path = os.path.join(data_dir,"temps.txt")
+    data_line = ""
+    with open(file_path, "r") as source_file:
+        data_line = source_file.readlines()[-1]
+    data_line = data_line.split(",")
+    timestamp = parser.parse(data_line[0])
+    value = float(data_line[1])
+    return timestamp, value
+
+def read_params():
+    global counter
+    if params_dir is not None:
+        file = open(params_dir+"twilio.txt","r")
+        data = file.read().strip()
+        list_params = data.split("\n")
+        for each in list_params:
+            holder = each.split("=")
+            main_params[holder[0]]=holder[1]
+        file.close()
+
+def voice_formatter(temperature):
+    temperature = str(temperature).split(".")
+    decimals = list(temperature[1])
+    return " ".join([temperature[0],"point",decimals[0],decimals[1],"degrees","Fahrenheit"])
+
+read_params()
+
+def panic(temp = None,time = None):
+    client = Client(main_params.get("AccountSID"), main_params.get("AuthToken"))
+    if temp is not None:
+        payload = "<Response><Say>Alert! Current temperature is " + voice_formatter(temp) + "</Say></Response>"
+    elif time is not None:
+        payload = "<Response><Say>Alert! It has been " + time + " minutes since last update, and temp was less than 50 degrees.</Say></Response>"
+    call = client.calls.create(
+                            twiml=payload,
+                            to='+19196077727',
+                            from_='+17865634668'
+                        )
+    print(call.sid)
+
+
+next_check = datetime.datetime.now()
+while True:
+    timenow = datetime.datetime.now()
+    if timenow > next_check:
+        alerted = False
+        timestamp, temperature = get_greenhouse_data()
+        if (timenow - timestamp) < datetime.timedelta(minutes = 4):
+            next_check = timestamp + datetime.timedelta(minutes=5)
+        elif (timenow - timestamp) > datetime.timedelta(minutes = 4):
+            if temperature < 50.0:
+                panic(time = (timenow - timestamp).minutes)
+                next_check = timenow + datetime.timedelta(minutes=5)
+                alerted = True
+        if temperature < 42.0 and not alerted:
+            panic(temp = temperature)
+            next_check = timenow + datetime.timedelta(minutes=5)
+            alerted = True
+    time.sleep(30)
--- Python/webserver/main.py
+++ Python/webserver/main.py
@@ -37,17 +37,12 @@ def read_params():
 
 def generate_plot(time_length, time_reference):
     if data_dir is not None:
-        file = open(data_dir +main_params["TempFile"],"r")
-        data = file.read().strip()
-        data_list = data.split("\n")
-        storage_dict = {}
-        for each in data_list:
-            holder = each.split(",")
-            time_holder = parser.parse(holder[0])
-            if ((time_reference - time_length) < time_holder) and (time_holder < time_reference):
-                storage_dict[time_holder] = float(holder[1])
-        dict_split = storage_dict.items()
-        dict_split = sorted(dict_split)
+        dict_holder = load_data_file(data_dir +main_params["TempFile"])
+        dict_split = []
+        for each in dict_holder:
+            if ((time_reference - time_length) < each[0]) and (each[0] < time_reference):
+                dict_split.append(each)
+        if len(dict_split) < 2: return "Insufficient Data from Time Period Given"
         x,y = zip(*dict_split)
         xtick_holder = np.linspace((time_reference-time_length).timestamp(),time_reference.timestamp(),8)
         xtick_holder = [datetime.datetime.fromtimestamp(each) for each in xtick_holder]
@@ -67,6 +62,20 @@ def generate_plot(time_length, time_reference):
             tick_counter += 1
         plt.savefig(os.path.join(os.path.abspath(os.path.dirname(__file__)),"static/graph.jpg"),dpi=600)
         plt.clf()
+        return 0
+
+def load_data_file(file_path):
+    file = open(file_path,"r")
+    data = file.read().strip()
+    data_list = data.split("\n")
+    storage_dict = {}
+    for each in data_list:
+        holder = each.split(",")
+        time_holder = parser.parse(holder[0])
+        storage_dict[time_holder] = float(holder[1])
+    dict_split = storage_dict.items()
+    dict_split = sorted(dict_split)
+    return dict_split
 
 def read_time_arg(input_string):
     holder_int = ""
@@ -85,15 +94,23 @@ def read_time_arg(input_string):
     elif holder_char == "w":
         return datetime.timedelta(weeks=holder_int)
     
-def preserve_arguments(arguments,keys=True):
+def preserve_arguments(arguments,keys=True,dict_ret=False,rem_graph=False):
     query = "?"
+    holder_dict = {}
     for each in arguments.keys():
         if (each in ["ReadKey","WriteKey"] and not keys):
             pass
+        elif (rem_graph and each in ["Time","EndDate"]):
+            pass
+        elif dict_ret:
+            holder_dict[each] = arguments[each]
         else:
             query += each + "=" + arguments[each]+"&"
     query = query[0:-1]
-    return query
+    if not dict_ret:
+        return query
+    else:
+        return holder_dict
 
 app = Flask(__name__)
 
@@ -145,7 +162,13 @@ def write_data():
         else:
             return render_template("writefail.html")
     else:
-        return redirect("/"+preserve_arguments(arguments,False))
+        return redirect("/"+preserve_arguments(arguments))
+
+@app.route("/now")
+def current_cond():
+    arguments = request.args.to_dict()
+    data_list = load_data_file(data_dir +main_params["TempFile"])
+    return render_template("current.html",TimeStamp=data_list[-1][0].strftime("%a %b %-d-%X").split("-"),Temperature=data_list[-1][1])
 
 @app.route("/image")
 def show_img():
@@ -154,10 +177,13 @@ def show_img():
     if arguments["ReadKey"] == main_params["ReadKey"]:
         image_name = str(counter).zfill(int(main_params["ImgNameLength"]))+".jpg"
         image_path=data_dir+"images/"+image_name
-        shutil.copy(image_path,(os.path.dirname(os.path.abspath(__file__))+"/static/image.jpg"))
-        holder = {"ImagePath":"static/image.jpg"}
-        counter += 1
-        return render_template("image.html",**holder)
+        if os.path.exists(image_path):
+            shutil.copy(image_path,(os.path.dirname(os.path.abspath(__file__))+"/static/image.jpg"))
+            holder = {"ImagePath":"static/image.jpg"}
+            counter += 1
+            return render_template("image.html",**holder)
+        else:
+            return render_template("error.html",Err_Desc="Image Path Does not Exist",Keys=preserve_arguments(arguments))
     else:
         return redirect("/"+preserve_arguments(arguments,False))
     
@@ -171,9 +197,13 @@ def show_graph():
             oldest_point = read_time_arg(arguments["Time"])
         if "EndDate" in arguments.keys():
             stop_point = parser.parse(arguments["EndDate"])
-        generate_plot(oldest_point,stop_point)
-        holder = {"ImagePath":"static/graph.jpg","Keys":preserve_arguments(arguments)}
-        return render_template("graph.html",**holder)
+        success = generate_plot(oldest_point,stop_point)
+        if success == 0:
+            holder = {"ImagePath":"static/graph.jpg","Keys":preserve_arguments(arguments)}
+            return render_template("graph.html",**holder, KeyDict=preserve_arguments(arguments,dict_ret=True,rem_graph=True))
+        else:
+            holder = {"Err_Desc":success,"Keys":preserve_arguments(arguments)}
+            return render_template("error.html",**holder)
     else:
         return redirect("/"+preserve_arguments(arguments,False))
 
